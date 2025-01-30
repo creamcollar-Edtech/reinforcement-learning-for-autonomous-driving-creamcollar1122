a6f73b9627bfee9483674b0e85709f866b0f99ed
kalpana <info@creamcollar.com>
2025-01-30 11:27:07 +0530

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -4,7 +4,7 @@ class LaneKeepingEnv:
     def __init__(self, road_width=10, lane_width=3):
         self.road_width = road_width
         self.lane_width = lane_width
-        self.position = road_width // 2  # test hello db Start in the cdddenter ofcc the road
+        self.position = road_width // 2  # test hello db Start in the cffdddenter ofcc the road
         self.done = False
         self.total_reward = 0
 
